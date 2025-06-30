0b4f8a69f5efcd55d17c6627692549cf8b2444ca
Ali Gamal <04aligamal@gmail.com>
2025-06-30 15:03:28 +0300

    Created draft of classes

---
+++ classes.cpp
@@ -0,0 +1,48 @@
+
+// Core classes
+
+class Item {
+    
+    
+};
+
+class Product : public Item {
+    
+    
+};
+
+class RawMaterial : public Item {
+    
+};
+
+class Equipment : public Item {
+    
+};
+
+class Supplier {
+    
+};
+
+class Transaction {
+    
+};
+
+// User classes
+
+class Admin {
+    
+};
+
+class InventoryManager {
+    
+};
+
+class StockClerk {
+    
+};
+
+// Maestro class
+
+class InventorySystem {
+    
+};
